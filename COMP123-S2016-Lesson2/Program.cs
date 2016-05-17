82a33b97597defc2a1f7c46666da56b7bfa725c3
Harry Dai <dz655618@live.com>
2016-05-17 12:55:25 -0400

    Added a reference to driver class from units

--- COMP123-S2016-Lesson2/Program.cs
+++ COMP123-S2016-Lesson2/Program.cs
@@ -4,10 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+/*
+ * Author: Harry Dai
+ * Date: 2016-05-17
+ * Date modified: 2016-05-17
+ * Description: Advanced Methods Demo for Lesson 2
+ * Version: 0.0.2 - Added a reference to driver class from units
+ */
+
+
 namespace COMP123_S2016_Lesson2
 {
+
+/*
+ * Main driver class for Lesson 2
+ * 
+ * @class: Program
+ */
+
     class Program
     {
+
+        /*
+         * Main method for the drive classes
+         * 
+         * @constructor Main
+         * @param (string[]) args
+         */
         static void Main(string[] args)
         {
         }
